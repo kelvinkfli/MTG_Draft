de5e4f02c5d60d76620d91ad2fa139b0d9fc142c
kelvinkfli <kelvinkfli@gmail.com>
2016-10-09 18:41:20 -0400

    Update database.js

--- bin/database.js
+++ bin/database.js
@@ -10,14 +10,14 @@ module.exports = {
 };
 
 function connect(){
-  MongoClient.connect(url, function(err, db) {
+  MongoClient.connect(url, function(err, db) { //this callback function will be placed in queue, and will be ready after
       if (err) {
         console.log('Unable to connect to the mongoDB server. Error:', err);
       }
       else {
         console.log('Connected to mongoDB.');
-        emitter.emit('connected', db);
+        emitter.emit('connected', db); //emits the message "connected" upon successful connection to mongoDB
       }
   });
-  return emitter;
+  return emitter; //because its asynchronous, this emitter is returned first, when database.connect() is run via api.js
 }
