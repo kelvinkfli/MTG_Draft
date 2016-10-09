b701d5ab7d9d835eb7d0ad3a0ae5d7614b25a95f
Ben Li <benpyli@gmail.com>
2016-10-09 03:55:04 -0400

    Move db dependencies to API using events to signal connection state

--- bin/api.js
+++ bin/api.js
@@ -1,37 +1,69 @@
-// Script to pull data from MTG API
-// var db = require('./database');
-var mtg = require('mtgsdk');  // get dependancies
-var exports = module.exports = {};  // set exports object
+var mtg = require('mtgsdk');  // get api functions
+var database = require('./database');
+var db;
 
+module.exports = {
+  seedCards: getCards,
+  seedCommons: getCommon,
+  seedUncommons: getUncommon,
+  seedRares: getRares,
+  generateBooster: generateBooster,
+  getBooster: getBooster,
+  getConnection: getConnection
+};
 
-//------------------FUNCTIONS TO BE EXPORTED BELOW-----------------------
-
-//Inserts all cards from set into database
-exports.getCards = function(collectionname) {
-    mtg.card.all({set: 'zen'}).on('data', function (card) {
-        collectionname.insert(card);
-    });
+function getConnection(){
+  connection = database.connect();
+  connection.on('connected', setDBConnection);
 }
 
-//Function that takes in 4 parameters, one for each database + booster database:
-// -randomly selects cards with rarity distribution and inserts into database
-exports.generateBooster = function(raredb, uncommondb, commondb, boosterdb) {
-    raredb.aggregate([{$sample: {size: 1}}]).forEach(function (card) {
-        boosterdb.insert(card);
-    })
-    uncommondb.aggregate([{$sample: {size: 3}}]).forEach(function (card) {
-        boosterdb.insert(card);
-    })
-    commondb.aggregate([{$sample: {size: 11}}]).forEach(function (card) {
-        boosterdb.insert(card);
-    })
+function setDBConnection(connection){
+  db = connection;
+  console.log('MongoDB connection established with API.')
 }
 
-//http://stackoverflow.com/questions/2824157/random-record-from-mongodb
-//write generateBooster.js in its own file <-- root URL (eventually will replace "hello world")
-/*
- generateBooster.generate
- within generatebooster.js
+//Inserts all cards from set into collection
+function getCards() {
+  mtg.card.all({set: 'zen'}).on('data', function (card) {
+    db.collection('zendikar_fullset').insert(card);
+  });
+}
 
- generate()
- */
+//Inserts all commons into its own collection
+function getCommon() {
+  db.collection('zendikar_fullset').find({rarity: "Common"}).forEach(function (card) {
+      db.collection('zendikar_common').insert(card);
+  });
+}
+
+//Inserts all uncommons into its own collection
+function getUncommon() {
+  db.collection('zendikar_fullset').find({rarity: "Uncommon"}).forEach(function (card) {
+      db.collection('zendikar_uncommon').insert(card);
+  });
+}
+
+//Inserts all rares into its own collection
+function getRares() {
+  db.collection('zendikar_fullset').find({rarity: "Rare"}).forEach(function (card) {
+      db.collection('zendikar_rares').insert(card);
+  });
+}
+
+//Randomly generates a booster pack and inserts into collection
+function generateBooster() {
+  db.collection('zendikar_rares').aggregate([{$sample: {size: 1}}]).forEach(function (card) {
+    db.collection('zendikar_booster').insert(card);
+  })
+  db.collection('zendikar_uncommon').aggregate([{$sample: {size: 3}}]).forEach(function (card) {
+    db.collection('zendikar_booster').insert(card);
+  })
+  db.collection('zendikar_common').aggregate([{$sample: {size: 11}}]).forEach(function (card) {
+    db.collection('zendikar_booster').insert(card);
+  })
+}
+
+function getBooster(){
+  //return db.collection('zendikar_booster').find();
+  return 'this is the generated booster!';
+}
