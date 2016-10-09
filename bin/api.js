f5b04dd39fe5801ee7a814eb3ba77a99d0217cf5
kelvinkfli <kelvinkfli@gmail.com>
2016-10-09 18:39:20 -0400

    Update api.js

--- bin/api.js
+++ bin/api.js
@@ -13,12 +13,12 @@ module.exports = {
 };
 
 function getConnection(){
-  connection = database.connect();
-  connection.on('connected', setDBConnection);
+  connection = database.connect();  //stores emitter so we are listening to events in database.js
+  connection.on('connected', setDBConnection); //event listener: when 'connected' message is received, run setDBConnection
 }
 
-function setDBConnection(connection){
-  db = connection;
+function setDBConnection(connection){ //these connection parameters are taken from line 19 of database.js
+  db = connection; //this variable holds an instance of our mongo connection, so we can use mongodb commands
   console.log('MongoDB connection established with API.')
 }
 
